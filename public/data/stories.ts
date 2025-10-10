287377f557bc6ddc59abc3cb85405660e2a1f3f8
Akeem Smith <asmith.339@outlook.com>
2025-10-10 08:07:04 -0400

    added stories and experiences sections, quiz button

---
+++ public/data/stories.ts
@@ -0,0 +1,15 @@
+// Mock data for local stories section on site pages (currently on The Soup Bowl page)
+
+export const stories = [
+    {
+        title: "Where the Atlantic Meets The Rock",
+        src: "/data/cat-meow-9-fx-306185.mp3"
+    },
+    {
+        title: "From Fishing Village to Surfer’s Haven",
+        src: "/data/cat-meow-9-fx-306185.mp3"
+    },
+    {
+        title: "Legends in the Breeze",
+        src: "/data/cat-meow-9-fx-306185.mp3"
+    }]
